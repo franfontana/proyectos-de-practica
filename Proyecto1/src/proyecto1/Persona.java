1ed2789a7477f15090b8c66e397d8e1f501f30fc
Francisco Fontana <francisco.fontana@mercadolibre.com>
2015-08-19 12:10:08 -0300

    Codigo descomentado Persona.java

--- Proyecto1/src/proyecto1/Persona.java
+++ Proyecto1/src/proyecto1/Persona.java
@@ -11,34 +11,34 @@ public class Persona {
 	private final int sobrepeso=1;
 
 	
-//	public boolean esMayorDeEdad(){
-//		if(edad >= 18){
-//			return true;
-//		}else{
-//			return false;
-//		}
-//	}
-//	
-//	public void comprobarSexo(char sexo){
-//		if(sexo != 'H' && sexo != 'M'){
-//			this.sexo = 'H';
-//			System.out.println("La persona es M");
-//			}else{
-//				this.sexo = sexo;
-//			}
-//	}
-//	
-//	public void generarDNI(){
-//		final int divisor=23;
-//		int numDNI=((int)Math.floor(Math.random()*(100000000-10000000)+10000000));
-//		int res=numDNI - (numDNI/divisor*divisor);
-//		char letraDNI=generaLetraDNI(res);
-//		DNI=Integer.toString(numDNI) + letraDNI;
-//	}
-//
-//	private char generaLetraDNI(int res) {
-//		// TODO Auto-generated method stub
-//		return 0;
-//	}
+	public boolean esMayorDeEdad(){
+		if(edad >= 18){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public void comprobarSexo(char sexo){
+		if(sexo != 'H' && sexo != 'M'){
+			this.sexo = 'H';
+			System.out.println("La persona es M");
+			}else{
+				this.sexo = sexo;
+			}
+	}
+	
+	public void generarDNI(){
+		final int divisor=23;
+		int numDNI=((int)Math.floor(Math.random()*(100000000-10000000)+10000000));
+		int res=numDNI - (numDNI/divisor*divisor);
+		char letraDNI=generaLetraDNI(res);
+		DNI=Integer.toString(numDNI) + letraDNI;
+	}
+
+	private char generaLetraDNI(int res) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
 
